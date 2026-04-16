af002bf6fa738161ba32526d1a73bf1cc28a01fc
Leonel <leoneljesus5m@gmail.com>
2026-04-16 14:02:02 -0300

    Primer commit con codigo

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header className="header">
+      <a href="/" className="logo">Logo</a>
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">Appointments</a>
+        <a href="/">Contact</a>
+        <a href="/">About</a>
+        <a href="/">Login</a>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
