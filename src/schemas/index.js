a0e49342c2c64c296787a585fa820f7af98e2989
Leonel <leoneljesus5m@gmail.com>
2026-04-24 04:27:53 -0300

    Solicitud POST para registro de usuarios completada

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -10,6 +10,15 @@ export const registerSchema = Yup.object({
   email: Yup.string()
     .email("El email no es válido")
     .required("El email es obligatorio"),
+  birthdate: Yup.date()
+    .required("La fecha es obligatoria")
+    .max(new Date(), "Ingrese una fecha válida")
+    .min(new Date("1920-01-01"), "Fecha no válida"),
+  dni: Yup.string()
+    .required("El DNI es obligatorio")
+    .matches(/^[0-9]+$/, "El DNI solo debe contener números")
+    .min(7, "El DNI debe tener al menos 7 dígitos")
+    .max(8, "El DNI no puede tener más de 8 dígitos"),
   username: Yup.string()
     .min(4, "El nombre de usuario debe contener al menos 4 caracteres")
     .max(15, "El nombre de usuario no puede contener más de 15 caracteres")
@@ -18,5 +27,8 @@ export const registerSchema = Yup.object({
     .min(8, "La contraseña debe contener al menos 8 caracteres")
     .max(20, "La contraseña no puede contener más de 20 caracteres")
     .required("La contraseña es obligatoria")
-    .matches(passwordRegEx, "La contraseña debe contener al menos una letra y un número")
+    .matches(
+      passwordRegEx,
+      "La contraseña debe contener al menos una letra y un número",
+    ),
 });
--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,64 +1,125 @@
 import { Formik, Form, Field } from "formik";
 import { registerSchema } from "../../schemas";
-import classes from './Register.module.css'
+import classes from "./Register.module.css";
+import axios from "axios"
 
 const initialValues = {
   name: "",
   email: "",
+  birthdate: "",
+  dni: "",
   username: "",
   password: "",
 };
 
-const onSubmit = (values, actions) => {
-  console.log(values);
-  actions.resetForm();
+const onSubmit = async (values, actions) => {
+  try {
+    const { dni, ...restOfValues } = values
+    const dataToSend = {
+      ...restOfValues,
+      nDni: Number(dni)
+    }
+    const response = await axios.post("http://localhost:3000/users/register", dataToSend)
+    alert("Registro exitoso!")
+    actions.resetForm()
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    alert(error.response?.data?.message || "Hubo un problema con el registro");
+  }
 };
 
 export default function Register() {
   return (
     <div>
-      <Formik initialValues={initialValues} validationSchema={registerSchema} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerSchema}
+        onSubmit={onSubmit}
+      >
         {({ errors, touched }) => (
           <Form className={classes.formContainer}>
-            <label htmlFor="name">Nombre</label>
-            <Field type="text" name="name" placeholder="Ingrese su nombre" className={classes.inputs}/>
-            <div className={classes.errorContainer}>
-              {errors.name && touched.name && (
-                <p className="formError">{errors.name}</p>
-              )}
+            <div className={classes.inputGroup}>
+              <label htmlFor="name">Nombre</label>
+              <Field
+                type="text"
+                name="name"
+                placeholder="Ingrese su nombre"
+                className={classes.inputs}
+              />
+              <div className={classes.errorContainer}>
+                {errors.name && touched.name && (
+                  <p className="formError">{errors.name}</p>
+                )}
+              </div>
             </div>
-            <label htmlFor="email">Email</label>
-            <Field
-              type="email"
-              name="email"
-              placeholder="example@example.com"
-              className={classes.inputs}
-            />
-            <div className={classes.errorContainer}>
-              {errors.email && touched.email && (
-                <p className="formError">{errors.email}</p>
-              )}
+            <div className={classes.inputGroup}>
+              <label htmlFor="email">Email</label>
+              <Field
+                type="email"
+                name="email"
+                placeholder="example@example.com"
+                className={classes.inputs}
+              />
+              <div className={classes.errorContainer}>
+                {errors.email && touched.email && (
+                  <p className="formError">{errors.email}</p>
+                )}
+              </div>
             </div>
-            <label htmlFor="username">Nombre de usuario</label>
-            <Field
-              type="text"
-              name="username"
-              placeholder="Ingrese su nombre de usuario"
-              className={classes.inputs}
-            />
-            <div className={classes.errorContainer}>
-              {errors.username && touched.username && (
-                <p className="formError">{errors.username}</p>
-              )}
+            <div className={classes.inputGroup}>
+              <label htmlFor="birthdate">Fecha de Nacimiento</label>
+              <Field type="date" name="birthdate" className={classes.inputs} />
+              <div className={classes.errorContainer}>
+                {errors.birthdate && touched.birthdate && (
+                  <p className="formError">{errors.birthdate}</p>
+                )}
+              </div>
             </div>
-            <label htmlFor="password">Contraseña</label>
-            <Field type="password" name="password" placeholder="************" className={classes.inputs}/>
-            <div className={classes.errorContainer}>
-              {errors.password && touched.password && (
-                <p className="formError">{errors.password}</p>
-              )}
+            <div className={classes.inputGroup}>
+              <label htmlFor="dni">Numero de DNI</label>
+              <Field
+                type="text"
+                name="dni"
+                placeholder="Ingrese su número de dni"
+                className={classes.inputs}
+              />
+              <div className={classes.errorContainer}>
+                {errors.dni && touched.dni && (
+                  <p className="formError">{errors.dni}</p>
+                )}
+              </div>
             </div>
-            <button type="submit" className={classes.btnForm}>Registro</button>
+            <div className={classes.inputGroup}>
+              <label htmlFor="username">Nombre de usuario</label>
+              <Field
+                type="text"
+                name="username"
+                placeholder="Ingrese su nombre de usuario"
+                className={classes.inputs}
+              />
+              <div className={classes.errorContainer}>
+                {errors.username && touched.username && (
+                  <p className="formError">{errors.username}</p>
+                )}
+              </div>
+            </div>
+            <div className={classes.inputGroup}>
+              <label htmlFor="password">Contraseña</label>
+              <Field
+                type="password"
+                name="password"
+                placeholder="************"
+                className={classes.inputs}
+              />
+              <div className={classes.errorContainer}>
+                {errors.password && touched.password && (
+                  <p className="formError">{errors.password}</p>
+                )}
+              </div>
+            </div>
+            <button type="submit" className={classes.btnForm}>
+              Registro
+            </button>
           </Form>
         )}
       </Formik>
