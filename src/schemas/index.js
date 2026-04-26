ce8695fef4c7a851ea95865a5eeb021aa2a2a43e
Leonel <leoneljesus5m@gmail.com>
2026-04-26 02:22:58 -0300

    Vista del login completada

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -34,5 +34,16 @@ export const registerSchema = Yup.object({
 });
 
 export const loginSchema = Yup.object({
-  
-})
+  username: Yup.string()
+    .min(4, "El nombre de usuario debe contener al menos 4 caracteres")
+    .max(15, "El nombre de usuario no puede contener más de 15 caracteres")
+    .required("El nombre de usuario es obligatorio"),
+  password: Yup.string()
+    .min(8, "La contraseña debe contener al menos 8 caracteres")
+    .max(20, "La contraseña no puede contener más de 20 caracteres")
+    .required("La contraseña es obligatoria")
+    .matches(
+      passwordRegEx,
+      "La contraseña debe contener al menos una letra y un número",
+    ),
+});
